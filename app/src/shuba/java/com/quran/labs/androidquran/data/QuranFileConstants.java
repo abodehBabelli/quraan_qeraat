c25315aea0b38caf9e994ca6f90b9b92d26409c2
abodehBabelli <a.babelli92@gmail.com>
2018-01-06 15:39:58 +0300

    Add Shuba flavor

---
+++ app/src/shuba/java/com/quran/labs/androidquran/data/QuranFileConstants.java
@@ -0,0 +1,27 @@
+package com.quran.labs.androidquran.data;
+
+import com.quran.labs.androidquran.ui.util.TypefaceManager;
+
+public class QuranFileConstants {
+  public static final String BASE_HOST = "http://api.quraan.faith/";
+  public static final String IMG_BASE_URL = BASE_HOST + "shuba/";
+  public static final String IMG_ZIP_BASE_URL = IMG_BASE_URL + "zips/";
+  public static final String PATCH_ZIP_BASE_URL = IMG_BASE_URL + "patches/v";
+  public static final String DATABASE_BASE_URL = IMG_BASE_URL + "databases/";
+  public static final String AYAHINFO_BASE_URL = IMG_BASE_URL + "databases/ayahinfo/";
+  public static final String AUDIO_DB_BASE_URL = IMG_BASE_URL + "databases/audio/";
+  public static final int FONT_TYPE = TypefaceManager.TYPE_UTHMANI_HAFS;
+
+  // local paths
+  public static final String QURAN_BASE = "quraan_qeraat/";
+  public static final String DATABASE_DIRECTORY = "databases/";
+  public static final String AUDIO_DIRECTORY = "shuba/audio/";
+  public static final String AYAHINFO_DIRECTORY = "shuba/" + DATABASE_DIRECTORY;
+  public static final String IMAGES_DIRECTORY = "shuba";
+
+  // arabic database
+  public static final String ARABIC_DATABASE = "quran.shuba.ar.db";
+
+  // images version
+  public static final int IMAGES_VERSION = 3;
+}
